2f9b564894f80c1b3ab55445d1c21769340204a2
Noel Lim <noellimx@gmail.com>
2022-12-06 01:05:29 +0800

    scaffold layour login form

---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,9 @@
+import "./LoginForm.css";
+
+export default () => {
+  return (
+    <div className="login-form-container">
+      <div className="login-form-body">Login Form</div>
+    </div>
+  );
+};
