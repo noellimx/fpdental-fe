25a6646b159a8e3d7d59abf320bed86555abebb4
Noel Lim <n.lim.4@y7nnx49n9t.ad.deliveryhero.com>
2022-12-08 10:21:51 +0800

    clean loggers

--- src/contexts/UserAppointments.tsx
+++ src/contexts/UserAppointments.tsx
@@ -4,14 +4,9 @@ import useAuthService from "./Auth";
 const useGeneralUserAppointmentService = (
   ctxAuth: ReturnType<typeof useAuthService>
 ) => {
-  console.log(`${JSON.stringify(ctxAuth)}`);
-
-  console.log(
-    `[useGeneralUserAppointmentService - ctxAuth]${JSON.stringify(ctxAuth)}`
-  );
   useEffect(() => {
     console.log(
-      `[useGeneralUserAppointmentService] auth status changed ${JSON.stringify(
+      `[useGeneralUserAppointmentService] auth context changed ${JSON.stringify(
         ctxAuth
       )}`
     );
