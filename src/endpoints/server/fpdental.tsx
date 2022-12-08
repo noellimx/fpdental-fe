ac36c5560a933db367ad85b1778c261124542df5
Noel Lim <n.lim.4@y7nnx49n9t.ad.deliveryhero.com>
2022-12-08 13:51:52 +0800

    TODO server.fpdental

--- src/endpoints/server/fpdental.tsx
+++ src/endpoints/server/fpdental.tsx
@@ -1,77 +1,75 @@
-// //
+//
 
-// export const dummyAPIServer = (() => {
-//   const _validateTokenWithServer = async (token: Token) => {
-//     return token && true;
-//   };
+import { HttpStatusCodes, Role } from "../../contexts/Auth";
+import { Appointment, UuidString } from "../../contexts/UserAppointments";
+import { dummyAPIBrowser, Token } from "../browser";
+export const dummyAPIServer = (() => {
+  const _validateTokenWithServer = async (token: Token) => {
+    // TODO
+    return token && true;
+  };
 
-//   const _getMyAppointments = async (token: Token) => {
-//     const getFn = DummyAppointments[token.username]?.get;
-//     const appointments = getFn ? getFn() : [];
-//     return appointments;
-//   };
+  const _getMyAppointments = async (token: Token): Promise<Appointment[]> => {
+    // TODO
+    const appointments: Appointment[] = [];
+    return appointments;
+  };
 
-//   const _removeAppointment = async (
-//     id: UuidString,
-//     token: Token
-//   ): Promise<boolean> => {
-//     const dbuser = DummyAppointments[token.username];
+  const _removeAppointment = async (
+    id: UuidString,
+    token: Token
+  ): Promise<boolean> => {
+    // TODO
+    return new Promise((resolve) => {
+      resolve(false);
+    });
+  };
+  return {
+    isValidToken: async () => {
+      const token = dummyAPIBrowser.getSessionToken();
+      const is = await _validateTokenWithServer(token);
+      return is ? { is, token } : { is, token: null };
+    },
+    getMyAppointments: async (): Promise<Appointment[]> => {
+      const token = dummyAPIBrowser.getSessionToken();
+      const appointments = await _getMyAppointments(token);
+      return appointments;
+    },
 
-//     return new Promise((resolve) => {
-//       if (dbuser) {
-//         resolve(dbuser.remove(id));
-//       } else {
-//         resolve(false);
-//       }
-//     });
-//   };
-//   return {
-//     isValidToken: async () => {
-//       const token = dummyAPIBrowser.getSessionToken();
-//       const is = await _validateTokenWithServer(token);
-//       return is ? { is, token } : { is, token: null };
-//     },
-//     getMyAppointments: async (): Promise<Appointment[]> => {
-//       const token = dummyAPIBrowser.getSessionToken();
-//       const appointments = await _getMyAppointments(token);
-//       return appointments;
-//     },
+    removeMyAppointment: async (apptId: UuidString) => {
+      const token = dummyAPIBrowser.getSessionToken();
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(_removeAppointment(apptId, token));
+        }, 300);
+      });
+    },
 
-//     removeMyAppointment: async (apptId: UuidString) => {
-//       const token = dummyAPIBrowser.getSessionToken();
-//       return new Promise((resolve) => {
-//         setTimeout(() => {
-//           resolve(_removeAppointment(apptId, token));
-//         }, 300);
-//       });
-//     },
+    login: async (
+      un: string,
+      pw: string
+    ): Promise<{ statusCode: number; token?: Token }> => {
+      // TODO
+      return new Promise((resolve) =>
+        setTimeout(() => {
+          console.log(`[API-login] ${un} `);
+          if (un === pw) {
+            let role = Role.UNKNOWN;
+            if (un === "dummyadmin") {
+              role = Role.ADMIN;
+            } else {
+              role = Role.GENERAL;
+            }
 
-//     login: async (
-//       un: string,
-//       pw: string
-//     ): Promise<{ statusCode: number; token?: Token }> => {
-//       return new Promise((resolve) =>
-//         setTimeout(() => {
-//           console.log(`[dummyAPI-login] ${un} `);
-//           if (un === pw) {
-//             let role = Role.UNKNOWN;
-//             if (un === "dummyadmin") {
-//               role = Role.ADMIN;
-//             } else {
-//               role = Role.GENERAL;
-//             }
-
-//             resolve({
-//               statusCode: HttpStatusCodes.OK,
-//               token: { id: `${un}12345`, username: un, role },
-//             });
-//           } else {
-//             resolve({ statusCode: 403 });
-//           }
-//         }, 1000)
-//       );
-//     },
-//   };
-// })();
-
-// export {};
+            resolve({
+              statusCode: HttpStatusCodes.OK,
+              token: { id: `${un}12345`, username: un, role },
+            });
+          } else {
+            resolve({ statusCode: 403 });
+          }
+        }, 1000)
+      );
+    },
+  };
+})();
